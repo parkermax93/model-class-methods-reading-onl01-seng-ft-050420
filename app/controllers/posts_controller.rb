b9eb7967981f20181ffd790f86c5f96548141d93
Parker Williamson <parker.williamson@gmail.com>
2020-07-27 19:29:28 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
   helper_method :params
   def index
-    # provide a list of authors to the view for the filter control
     @authors = Author.all
    
-    # filter the @posts list based on user input
     if !params[:author].blank?
       @posts = Post.where(author: params[:author])
     elsif !params[:date].blank?
